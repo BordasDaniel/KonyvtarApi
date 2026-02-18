761016b6dff3f3ac693a0b8deac58886caeffd34
Dániel Bordás <bordas.daniel0124@gmail.com>
2026-02-18 08:03:29 +0100

    asd

--- Controllers/TelepulesController.cs
+++ Controllers/TelepulesController.cs
@@ -51,7 +51,53 @@ namespace KonyvtarApi.Controllers
             }
         }
 
-        [HttpGet()]
+        [HttpDelete("TorolTelepules/{id}")]
+        public IActionResult DeleteTelepules(int id)
+        {
+            using (var context = new KonyvtarakContext())
+            {
+                try
+                {
+                    var keresett = context.Telepuleseks.FirstOrDefault(u => u.Id == id);
+                    Telepulesek telep = new()
+                    {
+                        Id = id
+                    };
+
+                    if (keresett != null)
+                    {
+                        context.Remove(telep);
+                        return Ok("Sikeres törlés!");
+                    }
+                    else
+                    {
+                        return NotFound("Nincs ilyen!");
+                    }
+
+
+                } catch(Exception ex) {
+                    return BadRequest("Sikertelen törlés");
+                }
+            }
+        }
+
+        [HttpGet("Resznev/{reszlet}")]
+        public IActionResult GetByReszlet(string reszlet)
+        {
+            using (var context = new KonyvtarakContext())
+            {
+                try
+                {
+                    var keresettek = context.Te
+
+                } catch(Exception ex)
+                {
+                    return BadRequest("Sikertelen lekérdezés");
+                }
+            }
+        }
+
+
 
 
     }
